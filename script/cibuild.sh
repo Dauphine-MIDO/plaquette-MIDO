8b768fe49d39f4b8b2d6e8952efbb147f8b0ce90
Barnabé Geffroy <barnabe.geffroy@psl.eu>
2020-05-05 11:39:11 +0200

    really solve issue #4

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -3,15 +3,21 @@ set -e
 REPO="receive_plaquette"
 FILE_LOG=out.log
 FILE_PDF=out.pdf
-DEPLOY_REPO="https://${ACCESS_TOKEN}@github.com/barnabegeffroy/${REPO}.git"
+DEPLOY_REPO="https://${ACCESS_TOKEN}@github.com/barnabegeffroy/${REPO}.git" 
 function main {
 	clean
+	get_current_doc
 	build_doc
 	if [ -z "$TRAVIS_PULL_REQUEST" ]; then
 	   	echo "except don't publish doc for pull requests"
     else 
 		deploy
 	fi  
+}
+
+function get_current_doc { 
+	echo "getting latest doc"
+	git clone --depth 1 $DEPLOY_REPO ../${REPO}
 }
 
 function clean { 
@@ -35,8 +41,8 @@ function deploy {
 	if test -f "${FILE_PDF}"; then
 		FILES="${FILES} ${FILE_PDF}"		
 	fi
-	mv -f ${FILES} ${REPO}
-	cd ${REPO}
+	mv -f ${FILES} ../${REPO}
+	cd ../${REPO}
 	git config --global user.name "Travis CI"
     git config --global user.email barnabe.geffroy@psl.eu
 	git add ${FILES}
