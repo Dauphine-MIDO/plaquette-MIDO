af33012128ef4fdce964f87df5e99b85212b77c1
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-05-15 12:08:14 +0200

    Some cosmetic improvement

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -4,7 +4,8 @@ REPO="receive_plaquette"
 FILE_LOG="out.log"
 FILE_PDF="out.pdf"
 DEPLOY_REPO="https://${ACCESS_TOKEN}@github.com/barnabegeffroy/${REPO}.git" 
-BUILT=0
+BUILT_EXIT_CODE=1
+
 function main {
 	clean
 	get_current_doc
@@ -18,42 +19,44 @@ function main {
 
 function clean { 
 	echo "cleaning docs"
-	if [ -f "${FILE_LOG}" ]; then rm -f ${FILE_LOG}; fi 
-	if [ -f "${FILE_PDF}" ]; then rm -f ${FILE_PDF}; fi 
+	if [ -f "${FILE_LOG}" ]; then rm -f "${FILE_LOG}"; fi 
+	if [ -f "${FILE_PDF}" ]; then rm -f "${FILE_PDF}"; fi 
 }
 
 function get_current_doc { 
 	echo "getting latest doc"
-	git clone --depth 1 $DEPLOY_REPO ../${REPO}
+	git clone --depth 1 $DEPLOY_REPO "../${REPO}"
 }
 
 function build_doc { 
 	echo "trying to build doc"
 	if	mvn exec:java -Dexec.mainClass=io.github.oliviercailloux.plaquette_mido_soap.M1ApprBuilder; then
 		echo "Build succeeded"
+		BUILT_EXIT_CODE=0
 	else
    		echo "Build failed"
-		BUILT=1
+		BUILT_EXIT_CODE=1
 	fi
 }
 
 function deploy {
 	echo "deploying changes"
-	mv -f "${FILE_LOG}" ../${REPO}
+	mv -f "${FILE_LOG}" "../${REPO}"
 	if test -f "${FILE_PDF}"; then
-		mv -f "${FILE_PDF}" ../${REPO}		
+		mv -f "${FILE_PDF}" "../${REPO}"
 	fi
-	cd ../${REPO}
-	git config --global user.name "Travis CI"
-    git config --global user.email barnabe.geffroy@psl.eu
+	cd "../${REPO}"
+	git config user.name "Travis CI"
+    git config user.email barnabe.geffroy@psl.eu
 	git add "${FILE_LOG}"
 	if test -f "${FILE_PDF}"; then
 		git add "${FILE_PDF}"		
 	fi
 	git status
-	git commit -m "Lastest doc built on travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
+	git commit -m "Lastest doc built on travis build $TRAVIS_BUILD_NUMBER auto-pushed to github (exit code ${BUILT_EXIT_CODE})"
 	git push $DEPLOY_REPO master
-	exit ${BUILT}
+	exit ${BUILT_EXIT_CODE}
 }
 
 main
+
