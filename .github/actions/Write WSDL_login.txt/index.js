099f78bbca53f7093cef9bba0bcb927d1f1c34a3
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-11-02 01:30:30 +0100

    Writing WSDL_login

--- .github/actions/Write WSDL_login.txt/index.js
+++ .github/actions/Write WSDL_login.txt/index.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 try {
 	const nameToGreet = 'Ol';
-	console.log('Hello ${nameToGreet}!');
-	fs.writeFile('helloworld.txt', 'Hello World!', (err) => {
+	console.log(`Hello ${nameToGreet}!`);
+	fs.writeFile('WSDL_login.txt', 'Hello World!', (err) => {
 		  if (err) throw err;
 	});
 	console.log('Written.');
