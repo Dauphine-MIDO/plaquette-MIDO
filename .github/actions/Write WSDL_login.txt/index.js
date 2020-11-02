06d2f970a6aeb06b21cd959aea4c2574f723ac5d
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-11-02 01:52:25 +0100

    Caps

--- .github/actions/Write WSDL_login.txt/index.js
+++ .github/actions/Write WSDL_login.txt/index.js
@@ -3,10 +3,10 @@ const github = require('@actions/github');
 const fs = require('fs');
 
 try {
-	const start = encodeURI(process.env.API_username);
+	const start = encodeURI(process.env.API_USERNAME);
 	console.log(`Start: ${start}.`);
-	console.log(`Start inline: ${encodeURI(process.env.API_username)}.`);
-	fs.writeFile('WSDL_login.txt', `encodeURI(https://process.env.API_username:process.env.API_password@*)`, (err) => {
+	console.log(`Start inline: ${encodeURI(process.env.API_USERNAME)}.`);
+	fs.writeFile('WSDL_login.txt', `encodeURI(https://process.env.API_USERNAME:process.env.API_PASSWORD@*)`, (err) => {
 		  if (err) throw err;
 	});
 	console.log('Written.');
