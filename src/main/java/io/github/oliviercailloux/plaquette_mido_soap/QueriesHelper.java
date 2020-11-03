36f91d0d37d01cff20d6d66622515289ca30158b
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-11-03 20:20:48 +0100

    Switch to JARiS library

--- src/main/java/io/github/oliviercailloux/plaquette_mido_soap/QueriesHelper.java
+++ src/main/java/io/github/oliviercailloux/plaquette_mido_soap/QueriesHelper.java
@@ -3,8 +3,8 @@ package io.github.oliviercailloux.plaquette_mido_soap;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 
-import io.github.oliviercailloux.creds_read.Credentials;
-import io.github.oliviercailloux.creds_read.CredsReader;
+import io.github.oliviercailloux.jaris.credentials.Credentials;
+import io.github.oliviercailloux.jaris.credentials.CredsReader;
 
 public class QueriesHelper {
 
