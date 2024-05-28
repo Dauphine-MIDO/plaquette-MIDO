6047e1ff1d62261b9065c8043b4251c3bc7eb109
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-05-28 10:32:41 +0200

    Rn pckg test

--- src/test/java/io/github/oliviercailloux/plaquette/QueryTests.java
+++ src/test/java/io/github/oliviercailloux/plaquette/QueryTests.java
@@ -1,4 +1,4 @@
-package io.github.oliviercailloux.plaquette_mido_soap;
+package io.github.oliviercailloux.plaquette;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
